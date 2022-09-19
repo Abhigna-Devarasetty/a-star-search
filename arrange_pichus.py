ac8103b77fc404db93d1f2820ae31fe9893e9fc4
Venkata Sai Abhigna Devarasetty <venkatasaiabhignadevarasetty@Venkatas-MacBook-Pro.local>
2022-09-18 23:23:37 -0400

    solved problem1 and problem 2:

--- arrange_pichus.py
+++ arrange_pichus.py
@@ -2,12 +2,10 @@
 #
 # arrange_pichus.py : arrange agents on a grid, avoiding conflicts
 #
-# Submitted by : [PUT YOUR NAME AND USERNAME HERE]
+# Submitted by : Venkata Sai Abhigna Devarasetty (vdevarase@iu.edu)
 #
 # Based on skeleton code in CSCI B551, Fall 2022.
-
 import sys
-
 # Parse the map from a given filename
 def parse_map(filename):
 	with open(filename, "r") as f:
@@ -23,15 +21,67 @@ def printable_house_map(house_map):
 
 # Add a pichu to the house_map at the given position, and return a new house_map (doesn't change original)
 def add_pichu(house_map, row, col):
-    return house_map[0:row] + [house_map[row][0:col] + ['p',] + house_map[row][col+1:]] + house_map[row+1:]
-
-# Get list of successors of given house_map state
+    a=house_map[0:row] + [house_map[row][0:col] + ['p',] + house_map[row][col+1:]] + house_map[row+1:]
+    #print(printable_house_map(a))
+    return a
+    
+#generating successors based on the house map,
+# checking diagonally, row and column
 def successors(house_map):
-    return [ add_pichu(house_map, r, c) for r in range(0, len(house_map)) for c in range(0,len(house_map[0])) if house_map[r][c] == '.' ]
+
+    length_row = len(house_map)
+    length_col = len(house_map[0])
+    flag_variable = True
+    new_list = []
+    for row in range(0,length_row):
+        for column in range(0,length_col):
+            flag_variable = True
+            if house_map[row][column] == '.':
+                for i,j in zip(range(0,length_row),range(column-1,-1,-1)):
+                    if i!=row:
+                        if house_map[i][j] == 'p':
+                            flag_variable = False
+                            break
+                        if house_map[i][j] == 'X':
+                            break
+                        if house_map[row][j] == '@':
+                            break
+
+                for i,j in zip(range(0,length_row),range(column+1,length_col)):
+                        if i!=row:
+                            if house_map[i][j] == 'p':
+                                flag_variable = False
+                                break
+                            if house_map[i][j] == 'X':
+                                break
+                            if house_map[row][j] == '@':
+                                break
+                for i in range(0,length_row):
+                        if i!=row:
+                            if house_map[i][column] == 'p':
+                                flag_variable = False
+                                break
+                            if house_map[i][column] == 'X':
+                                break
+                            if house_map[row][j] == '@':
+                                break
+                for j in range(0,length_col):
+                        if j!=column:
+                            if house_map[row][j] == 'p':
+                                flag_variable = False
+                                break
+                            if house_map[row][j] == 'X':
+                                break
+                            if house_map[row][j] == '@':
+                                break
+                if (flag_variable):
+                    new_list.append(add_pichu(house_map, row, column))
+    print(new_list)
+    return new_list
 
 # check if house_map is a goal state
 def is_goal(house_map, k):
-    return count_pichus(house_map) == k 
+    return count_pichus(house_map) == k
 
 # Arrange agents on the map
 #
@@ -47,7 +97,10 @@ def solve(initial_house_map,k):
             if is_goal(new_house_map,k):
                 return(new_house_map,True)
             fringe.append(new_house_map)
-
+            
+    #False is returned when we can't find a solution
+    if len(fringe) == 0:
+        return(0,False)
 # Main Function
 if __name__ == "__main__":
     house_map=parse_map(sys.argv[1])
@@ -57,5 +110,3 @@ if __name__ == "__main__":
     solution = solve(house_map,k)
     print ("Here's what we found:")
     print (printable_house_map(solution[0]) if solution[1] else "False")
-
-
--- route_pichu.py
+++ route_pichu.py
@@ -1,13 +1,11 @@
+
 #!/usr/local/bin/python3
 #
 # route_pichu.py : a maze solver
 #
-# Submitted by : [PUT YOUR NAME AND USERNAME HERE]
-#
-# Based on skeleton code provided in CSCI B551, Fall 2022.
+# Based on skeleton code provided in CSCI B551, Fall 2021.
 
 import sys
-
 # Parse the map from a given filename
 def parse_map(filename):
         with open(filename, "r") as f:
@@ -15,14 +13,15 @@ def parse_map(filename):
                 
 # Check if a row,col index pair is on the map
 def valid_index(pos, n, m):
-        return 0 <= pos[0] < n  and 0 <= pos[1] < m
+        return (0 <= pos[0] < n)  and (0 <= pos[1] < m)
 
-# Find the possible moves from position (row, col)
+# Find the possible moves from position (row, col) UUURRDDDRRUURRDD
 def moves(map, row, col):
         moves=((row+1,col), (row-1,col), (row,col-1), (row,col+1))
-
+        
         # Return only moves that are within the house_map and legal (i.e. go through open space ".")
-        return [ move for move in moves if valid_index(move, len(map), len(map[0])) and (map[move[0]][move[1]] in ".@" ) ]
+        return [ move for move in moves if valid_index(move, len(map), len(map[0])) and (map[move[0]][move[1]] in ".@") ]
+
 
 # Perform search on the map
 #
@@ -35,16 +34,67 @@ def moves(map, row, col):
 
 def search(house_map):
         # Find pichu start position
-        pichu_loc=[(row_i,col_i) for col_i in range(len(house_map[0])) for row_i in range(len(house_map)) if house_map[row_i][col_i]=="p"][0]
-        fringe=[(pichu_loc,0)]
+        length_row = len(house_map)
+        length_col = len(house_map[0])
+        pichu_loc=[]
+        # pichu_loc=[(row_i,col_i) for col_i in range(len(house_map[0])) for row_i in range(len(house_map)) if house_map[row_i][col_i]=="p"][0]
+        for column in range(0,length_col):
+                for row in range(0,length_row):
+                   if house_map[row][column]=="p":
+                        pichu_loc=[(row,column)][0]
+        #storing visited position as 1's and unvisited as 0's 
 
-        while fringe:
-                (curr_move, curr_dist)=fringe.pop()
-                for move in moves(house_map, *curr_move):
-                        if house_map[move[0]][move[1]]=="@":
-                                return (7, 'DDDDDDD')  # return a dummy answer
-                        else:
-                                fringe.append((move, curr_dist + 1))
+        visited = [[0] * len(house_map[0]) for _ in range(len(house_map[1]))]
+        #path to store the directions taken by each position
+        desired_path=""
+        fringe=[(pichu_loc,0,desired_path)] # fringe stores the location, distance and its path
+        prev_node={pichu_loc:(0,0)}
+        while len(fringe) > 0:
+                #popping the elements from fringe
+                (curr_move, curr_dist,path)=fringe.pop()
+                p=prev_node[curr_move]  #getting a parent from its position from dictionary
+                #calculating moves for children from parent position
+                x2 =  curr_move[0] - p[0]
+                y2 =  curr_move[1] - p[1]
+                
+                #storing the moves based on parent and child position
+                if x2 == 1 and y2 == 0:
+                    path+="D"
+                elif x2 == -1 and y2 == 0:
+                    path+="U"
+                elif x2 == 0 and y2 == -1:
+                    path+="L"
+                elif x2 == 0 and y2 == 1 :
+                    path+="R"
+                
+                if y2 == 0:
+                        if x2 == 1:
+                                path+="D"
+                        if x2 == -1:
+                                path+="U"   
+                elif x2 == 0:
+                        if y2 ==-1:
+                                path+="L"
+                        if y2 == 1:
+                                path+="R"
+                 
+                if house_map[curr_move[0]][curr_move[1]]=="@":
+                    return (curr_dist,path)
+                
+                visited[curr_move[0]][curr_move[1]]=1 
+                
+                for individual_move in moves(house_map, *curr_move): 
+                
+                    if visited[individual_move[0]][individual_move[1]] == 0:
+                        prev_node[individual_move]=curr_move 
+                        
+                        fringe.append((individual_move, curr_dist + 1,path))
+                        fringe.sort(key = lambda sort_variable: sort_variable[1])
+                        fringe[::-1]
+        
+        if len(fringe) == 0:
+                return (-1,"")
+                    
 
 # Main Function
 if __name__ == "__main__":
@@ -53,4 +103,3 @@ if __name__ == "__main__":
         solution = search(house_map)
         print("Here's the solution I found:")
         print(str(solution[0]) + " " + solution[1])
-
